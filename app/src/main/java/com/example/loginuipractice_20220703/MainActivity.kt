a22c0f99408868887e8fa6063331456fb8e57ffc
김대정 <daejkimkdj@gamil.com>
2022-07-03 13:29:10 +0900

    로그인 로직실행

--- app/src/main/java/com/example/loginuipractice_20220703/MainActivity.kt
+++ app/src/main/java/com/example/loginuipractice_20220703/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.loginuipractice_20220703
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
+        signUpBtn.setOnClickListener {
+            Toast.makeText(this, "회원가입버튼 클릭" , Toast.LENGTH_SHORT).show()
+        }
+//        로그인 로직실행
+        loginBtn.setOnClickListener { 
+            val id = idEdt.text.toString()
+            val password = passwordEdt.text.toString()
+            if(id != "admin"){
+                Toast.makeText(this , "아이디가 잘못됐습니다." , Toast.LENGTH_SHORT).show()
+            }else{
+                if(password != "asdf"){
+                    Toast.makeText(this , "비밀번호가 잘못됐습니다." , Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this , "관리자님 환영합니다.." , Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
     }
 }
